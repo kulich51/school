06078433b0685edd3b6b9e66a01709276e6b9ae2
kulich51 <59617932+kulich51@users.noreply.github.com>
2022-03-19 22:39:34 +0300

    Lesson 4.2. HW fixed after reviews

--- scripts421.sql
+++ scripts421.sql
@@ -1,6 +1,8 @@
 alter table student add constraint age_greater_16 check ( age > 16 );
 
-alter table student add constraint name_pk primary key (name);
+alter table student add constraint unique_name unique (name);
+
+alter table student alter column name set not null;
 
 alter table student alter column age set default 20;
 
