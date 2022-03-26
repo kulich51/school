49cec038407322fb01e61cad1aeaded0bfbc88a8
kulich51 <59617932+kulich51@users.noreply.github.com>
2022-03-26 18:01:45 +0300

    Lesson 4.4 Profiles. HW fixed after review

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,18 +1,18 @@
 package ru.hogwarts.school.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hogwarts.school.service.InfoService;
 
 @RestController
-@Profile("dev")
 public class InfoController {
 
-    @Autowired
-    private InfoService infoService;
+    private final InfoService infoService;
+
+    public InfoController(InfoService infoService) {
+        this.infoService = infoService;
+    }
 
     @GetMapping("getPort")
     public ResponseEntity<Integer> getPort() {
